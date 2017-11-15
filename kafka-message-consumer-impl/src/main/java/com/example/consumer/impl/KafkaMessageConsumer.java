b25199b81682f6f1c8b1f03533492db87a2970b9
Ignasi Marimon-Clos <ignasi@lightbend.com>
2017-11-15 19:20:01 +0100

    Emits one 'Done' per incoming message effectively expanding the groupedWithin

--- kafka-message-consumer-impl/src/main/java/com/example/consumer/impl/KafkaMessageConsumer.java
+++ kafka-message-consumer-impl/src/main/java/com/example/consumer/impl/KafkaMessageConsumer.java
@@ -2,6 +2,7 @@ package com.example.consumer.impl;
 
 import akka.Done;
 import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Source;
 import com.example.generator.api.KafkaMessageGeneratorService;
 import com.example.generator.api.Message;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import scala.concurrent.duration.FiniteDuration;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -27,17 +29,23 @@ public class KafkaMessageConsumer {
     }
 
     void start() {
+        int groupSize = 50;
+
         if (started.compareAndSet(false, true)) {
             kafkaMessageGeneratorService.messages()
                     .subscribe()
                     .atLeastOnce(
                             Flow.<Message>create()
-                                    .groupedWithin(2, new FiniteDuration(1, TimeUnit.SECONDS))
+                                    .groupedWithin(groupSize, new FiniteDuration(1, TimeUnit.SECONDS))
 //                                    .groupedWithin(1, new FiniteDuration(1, TimeUnit.SECONDS))
                                     .mapAsync(1, (List<Message> m) -> {
                                         log.info("Consumed message: " + m);
                                         return CompletableFuture.completedFuture(Done.getInstance());
-                                    })
+                                    }
+                                    ).flatMapConcat(
+                                            ignored ->
+                                                    Source.range(1, groupSize).map( i -> Done.getInstance())
+                            )
                     );
 
         }
